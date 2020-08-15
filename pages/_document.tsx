7ffc83983b7571da9ff363ccc5a1248e620a4d9d
marctorrelles <marctorrelles@gmail.com>
2020-08-15 16:27:43 +0200

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,7 @@ export default class MyDocument extends Document {
             }
           `}
           </style>
+          <link rel='shortcut icon' href='/favicon.png' />
         </Head>
         <body>
           <Main />
