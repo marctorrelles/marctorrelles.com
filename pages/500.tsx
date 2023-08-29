d06ac6a522e4b59956427fb497393e9ebcedf02a
Marc Torrelles <marctorrelles@gmail.com>
2023-08-29 17:09:03 +0200

    404 and 500 pages

---
+++ pages/500.tsx
@@ -0,0 +1,23 @@
+import PageContainer from "../components/organisms/PageContainer"
+import Title from "../components/atoms/Title"
+import Text from "../components/atoms/Text"
+import Link from "../components/atoms/Link"
+import { Links } from "../components/organisms/Nav"
+import Image from "next/image"
+
+const Home = () => (
+  <PageContainer>
+    <Title>500 - Server error</Title>
+    <Image
+      width="1920"
+      height="1080"
+      priority
+      quality={100}
+      src="/server-error.gif"
+      alt="Server error"
+    />
+    <Link href={Links.Home}>Go back home</Link>
+  </PageContainer>
+)
+
+export default Home
