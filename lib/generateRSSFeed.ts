ccd2f0acf04b4d0c4c6bc19fa0a27e2733369535
Marc Torrelles <marctorrelles@gmail.com>
2023-09-06 00:03:49 +0200

    Add RSS

---
+++ lib/generateRSSFeed.ts
@@ -0,0 +1,30 @@
+import fs from "fs"
+import RSS from "rss"
+import getSortedPosts from "./getSortedPosts"
+
+export default async function generateRssFeed() {
+  const siteUrl = process.env.VERCEL_URL
+  const posts = await getSortedPosts()
+
+  const feedOptions = {
+    title: "marctorrelles blog",
+    site_url: siteUrl,
+    feed_url: `${siteUrl}/rss.xml`,
+    image_url: `${siteUrl}/favicon.png`,
+    pubDate: new Date(),
+    copyright: `All rights reserved ${new Date().getFullYear()}, Marc Torrelles`,
+  }
+
+  const feed = new RSS(feedOptions)
+
+  posts.map((post) => {
+    feed.item({
+      title: post.title,
+      description: post.short,
+      url: `${siteUrl}/posts/${post.slug}`,
+      date: post.date,
+    })
+  })
+
+  fs.writeFileSync("./public/rss.xml", feed.xml({ indent: true }))
+}
